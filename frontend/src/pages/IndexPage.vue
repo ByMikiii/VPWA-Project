b4c347377419165de6ecb9247f543f5004f966b6
ByMikiii <mikixr1@gmail.com>
2025-09-26 01:37:20 +0200

    Add Dockerfiles & docker compose

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,43 +1,32 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page padding>
+    <q-btn label="Backend Request" @click="fetchUsers" />
+    <div v-if="users.length">
+      <div v-for="user in users" :key="user.id">
+        {{ user.name }} ({{ user.email }})
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import { ref } from 'vue'
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
-]);
+interface User {
+  id: number
+  name: string
+  email: string
+}
 
-const meta = ref<Meta>({
-  totalCount: 1200,
-});
+const users = ref<User[]>([])
+
+async function fetchUsers() {
+  try {
+    const res = await fetch('http://localhost:3333/')
+    users.value = await res.json()
+    console.log(users.value)
+  } catch (err) {
+    console.error('Fetch error:', err)
+  }
+}
 </script>
