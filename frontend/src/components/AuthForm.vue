156a2261314c9466bb2c4c3d422a18f00f3af26f
HP <tomas.beres9@gmail.com>
2025-10-25 17:07:19 +0200

    Add login with minimum styles

---
+++ frontend/src/components/AuthForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-form @submit.prevent="onSubmit">
+    <q-input
+      class="accent-input"
+      v-model="email"
+      label="Email"
+      type="email"
+      color="accent"
+      label-color="accent"  
+      text-color="accent"
+      outlined
+      field-border-color="accent"
+      required
+    />
+    <q-input
+      v-model="password"
+      label="Password"
+      type="password"
+      color="accent"
+      label-color="accent"  
+      text-color="accent"
+      outlined
+      required
+    />
+    <q-btn type="submit" label="Login" />
+  </q-form>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const email = ref('');
+    const password = ref('');
+
+    const emit = defineEmits(['submit']);
+
+    function onSubmit() {
+        emit('submit', { email: email.value, password: password.value });
+    }
+</script>
+
+<style lang="scss">
+       
+</style>
