cd753bd8d046679150d848d098ee2c91681b49e6
ByMikiii <mikixr1@gmail.com>
2025-10-26 23:08:27 +0100

    Add arrow back in profile

--- frontend/src/pages/ProfilePage.vue
+++ frontend/src/pages/ProfilePage.vue
@@ -1,28 +1,54 @@
 <template>
   <div class="q-pa-md flex flex-center">
     <q-card class="q-pa-lg bg-dark text-accent" style="max-width: 500px; width: 100%;">
-      
-      <q-card-section  class="text-right" dark>
-        <q-btn 
-        rounded outlined
-        color="accent"
-        text-color="primary" 
-        class="q-px-lg"
-        @click=logout>
-        Log out
-        </q-btn>
-      </q-card-section>
+      <div class="row items-center justify-between">
+        <q-card-section  class="text-right" dark>
+          <q-btn
+          rounded outlined
+          color="accent"
+          text-color="primary"
+          class="q-px-lg"
+          @click=back>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M9 14l-4 -4l4 -4" />
+            <path d="M5 10h11a4 4 0 1 1 0 8h-1" />
+          </svg>
+          </q-btn>
+        </q-card-section>
+
+
+        <q-card-section  class="text-right" dark>
+          <q-btn
+          rounded outlined
+          color="accent"
+          text-color="primary"
+          class="q-px-lg"
+          @click=logout>
+          Log out
+          </q-btn>
+        </q-card-section>
+      </div>
 
       <div class="flex justify-center q-mb-md">
-        <ProfilePicture 
+        <ProfilePicture
           :status="user.status"
           style="width: 120px; height: 120px; border-radius: 50%; object-fit: cover;"
         />
       </div>
 
       <div class="flex justify-center"><StatusDropdown></StatusDropdown></div>
-      
-      
+
+
 
       <q-card-section>
         <q-list padding class="text-center">
@@ -45,19 +71,19 @@
 
 
       <q-card-section class="row justify-between">
-        <q-btn 
-          rounded 
-          outlined 
-          color="accent" 
-          text-color="primary" 
+        <q-btn
+          rounded
+          outlined
+          color="accent"
+          text-color="primary"
           label="Edit profile"
-          @click="$router.push('/editProfile')" 
+          @click="$router.push('/editProfile')"
         />
-        <q-btn 
-          rounded 
-          outlined 
-          color="accent" 
-          text-color="primary" 
+        <q-btn
+          rounded
+          outlined
+          color="accent"
+          text-color="primary"
           label="Change password"
           @click="$router.push('/changePassword')"
         />
@@ -93,6 +119,10 @@
       };
 
       await router.push('/login');
+    }
+
+    const back = async () => {
+      await router.push('/');
     }
 
 </script>
